eafe88cb2614e2cc37a69f70b44e69338571d8d2
demirmehmet0 <demirmehmet0@yahoo.com>
2024-04-19 14:54:44 +0300

    fix: Sort action move to web from api

--- src/controllers/web/ItemController.php
+++ src/controllers/web/ItemController.php
@@ -388,6 +388,17 @@ class ItemController extends Controller
         }
     }
 
+    public function actionSort()
+    {
+        if (!\Yii::$app->user->can('menuWebItemSort')) {
+            throw new \yii\web\ForbiddenHttpException(Module::t('You are not allowed to access this page.'));
+        }
+        Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $data = Yii::$app->request->post();
+        MenuItem::sort($data);
+        return "success";
+    }
+
     /**
      * Finds the MenuItem model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
